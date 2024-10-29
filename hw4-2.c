a06962a640ad4223147348bb6261368fb78398ba
Jaisakthi Chandrakumar <am24101@shibaura-it.ac.jp>
2024-10-29 20:13:40 +0900

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int x;
+    printf("Test Case 1\nInput:\n");
+    scanf("%d", &x);
+    
+    printf("Output:\n");
+    if (x > 0)
+    {
+        printf("Positive\n");
+    }
+    else
+    {
+        printf("Negative or zero\n");
+    }
+    
+    return 0;
+}
