bebcab7a9c3ed5e1e9600237ae8e69b1e0112160
Jaisakthi Chandrakumar <am24101@shibaura-it.ac.jp>
2024-10-29 20:15:01 +0900

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int x, y;
+    printf("Test Case 1\nInput:\n");
+
+    printf("x? ");
+    scanf("%d", &x);
+    printf("y? ");
+    scanf("%d", &y);
+
+    printf("Output:\n");
+    if (x > y) {
+        printf("%d\n", x);
+    } else {
+        printf("%d\n", y);
+    }
+
+    return 0;
+}
