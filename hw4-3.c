1134426adac3448f94cb63dbe419e450e01c729c
Jaisakthi Chandrakumar <am24101@shibaura-it.ac.jp>
2024-10-29 20:14:16 +0900

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+    double p, q, h;
+    printf("Test Case 1\nInput:\n");
+    
+    printf("p? ");
+    scanf("%lf", &p);
+    
+    q = 1 - p;
+    h = -p * (log(p) / log(2)) - q * (log(q) / log(2));
+    
+    printf("Output:\n%.6lf\n", h);
+    
+    return 0;
+}
