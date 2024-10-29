2135f4c209365e0665bef9395d4a999a07e514e7
Jaisakthi Chandrakumar <am24101@shibaura-it.ac.jp>
2024-10-29 20:12:57 +0900

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int x, y, z, a;
+    
+    printf("Test Case 1\nInput:\n");
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    
+    a = (x + y + z) / 3;
+    printf("Output:\nAverage is %d\n", a);
+
+    return 0;
+}
